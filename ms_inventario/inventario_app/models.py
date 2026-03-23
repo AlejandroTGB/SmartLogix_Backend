2b2aa4d865a678924c4b0e16da2d192ad9cb0f56
Alejandro Torres <alej.torresg@duocuc.cl>
2026-03-23 17:23:07 -0300

    feat: Crear modelo Producto con validacion de precio y stock

--- ms_inventario/inventario_app/models.py
+++ ms_inventario/inventario_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+sku = models.CharField(max_length=50, unique=True)
+nombre = models.CharField(max_length=200)
+descripcion = models.TextField(blank=True, null=True)
+precio = models.DecimalField(max_digits=10, decimal_places=2,validators=[MinValueValidator(0.00)])
+stock = models.PositiveBigIntegerField(default=0)
+foto_url = models.URLField(blank=True, null=True)
+
+def __str__(self):
+    return f"{self.sku} - {self.nombre}"
