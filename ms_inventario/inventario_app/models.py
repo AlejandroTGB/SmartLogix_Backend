d414cc0e0c2e30899ef95075fe63e6005cb35e94
Alejandro Torres <alej.torresg@duocuc.cl>
2026-03-23 17:31:54 -0300

    fix: Correxion sintaxis class

--- ms_inventario/inventario_app/models.py
+++ ms_inventario/inventario_app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-sku = models.CharField(max_length=50, unique=True)
-nombre = models.CharField(max_length=200)
-descripcion = models.TextField(blank=True, null=True)
-precio = models.DecimalField(max_digits=10, decimal_places=2,validators=[MinValueValidator(0.00)])
-stock = models.PositiveBigIntegerField(default=0)
-foto_url = models.URLField(blank=True, null=True)
+class Producto(models.Model):
+    sku = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)])
+    stock = models.PositiveBigIntegerField(default=0)
+    foto_url = models.URLField(blank=True, null=True)
 
-def __str__(self):
-    return f"{self.sku} - {self.nombre}"
+    def __str__(self):
+        return f"{self.sku} - {self.nombre}"
